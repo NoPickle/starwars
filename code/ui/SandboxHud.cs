94576d2b6274c2050bdc4ce57c8695871d76b4ca
Hamo <91020563+NoPickle@users.noreply.github.com>
2021-09-19 21:59:01 +0200

    Update SandboxHud.cs

--- code/ui/SandboxHud.cs
+++ code/ui/SandboxHud.cs
@@ -19,7 +19,7 @@ public partial class SandboxHud : HudEntity<RootPanel>
 		RootPanel.AddChild<VoiceList>();
 		RootPanel.AddChild<KillFeed>();
 		RootPanel.AddChild<Scoreboard<ScoreboardEntry>>();
-		// RootPanel.AddChild<Health>();
+		RootPanel.AddChild<Health>();
 		RootPanel.AddChild<InventoryBar>();
 		RootPanel.AddChild<CurrentTool>();
 		RootPanel.AddChild<SpawnMenu>();
