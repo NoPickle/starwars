1960adee93b70199a4412cabcbfd44ab1cdaf72e
Hamo <91020563+NoPickle@users.noreply.github.com>
2021-09-20 11:09:19 +0200

    FIX

--- code/ui/InventoryIcon.cs
+++ code/ui/InventoryIcon.cs
@@ -8,39 +8,16 @@ public class InventoryIcon : Panel
 	public Entity TargetEnt;
 	public Label Label;
 	public Label Number;
-	public int index;
-	public bool clicked;
 
 	public InventoryIcon( int i, Panel parent )
 	{
-		index = i;
 		Parent = parent;
 		Label = Add.Label( "empty", "item-name" );
 		Number = Add.Label( $"{i}", "slot-number" );
-
-		AddEventListener( "OnClick", ()=>
-		{
-			clicked = true;
-		} );
-	}
-
-	[Event( "buildinput" )]
-	public void ProcessClientInput( InputBuilder input )
-	{
-		if(clicked)
-		{
-			clicked = false;
-
-			if(Local.Pawn is Player p)
-				if(p.Inventory != null)
-					if(p.Inventory.GetSlot(index-1) != null)
-						input.ActiveChild = p.Inventory.GetSlot( index-1 );
-		}
 	}
 
 	public void Clear()
 	{
-		Style.SetBackgroundImage("");
 		Label.Text = "";
 		SetClass( "active", false );
 	}
