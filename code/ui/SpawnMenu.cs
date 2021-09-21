d5c8b25a9ec82b84aec1461ddea3f3c710980b4c
RUD3R <91020563+NoPickle@users.noreply.github.com>
2021-09-21 23:54:23 +0200

    r

--- code/ui/SpawnMenu.cs
+++ code/ui/SpawnMenu.cs
@@ -35,6 +35,9 @@ public partial class SpawnMenu : Panel
 				var wep = body.AddChild<WeaponList>();
 				tabs.AddButtonActive( "🔫 Weapons", ( b ) => wep.SetClass( "active", b ) );
 
+				var props = body.AddChild<SpawnList>();
+				tabs.SelectedButton = tabs.AddButtonActive( "🛡️ StarWars", ( b ) => propsr.SetClass( "active", b ) );				
+
 			}
 		}
 
