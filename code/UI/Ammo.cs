201f77f36ad26e2da3041f0dcc06432039534807
Hamo <91020563+NoPickle@users.noreply.github.com>
2021-09-21 21:37:58 +0200

    Update Ammo.cs

--- code/UI/Ammo.cs
+++ code/UI/Ammo.cs
@@ -18,7 +18,7 @@ public class Ammo : Panel
 		var player = Local.Pawn;
 		if ( player == null ) return;
 
-		var weapon = player.ActiveChild as BaseDmWeapon;
+		var weapon = player.ActiveChild as BaseWeapon;
 		SetClass( "active", weapon != null );
 
 		if ( weapon == null ) return;
