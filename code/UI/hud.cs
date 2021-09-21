5e5a7c5833e0defa2b653008531b562d88d6eb8e
Hamo <91020563+NoPickle@users.noreply.github.com>
2021-09-21 21:02:39 +0200

    FIX

--- code/UI/hud.cs
+++ code/UI/hud.cs
@@ -2,14 +2,14 @@
 using Sandbox.UI;
 
 [Library]
-public partial class SandboxHud : HudEntity<RootPanel>
+public partial class hud : HudEntity<RootPanel>
 {
-	public SandboxHud()
+	public hud()
 	{
 		if ( !IsClient )
 			return;
 
-		RootPanel.StyleSheet.Load( "/ui/SandboxHud.scss" );
+		RootPanel.StyleSheet.Load( "/ui/hud.scss" );
 
 		RootPanel.AddChild<NameTags>();
 		RootPanel.AddChild<CrosshairCanvas>();
